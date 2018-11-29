022db44db7c45c91ea6c87a4ad33347c196860bc
tungmk97 <tungnv975@gmail.com>
2018-11-29 16:09:30 +0700

    Refer to task 3906: Login

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -16,6 +16,7 @@ class UsersController < ApplicationController
     @user = User.new user_params
 
     if @user.save
+      log_in @user
       flash[:info] = t ".success"
       redirect_to @user
     else
