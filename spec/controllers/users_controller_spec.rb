bb5f898d9aa2e272bd920b7c44f453686c86c133
tungmk97 <tungnv975@gmail.com>
2018-11-29 16:09:30 +0700

    Refer to task 3905: Sign up

---
+++ spec/controllers/users_controller_spec.rb
@@ -0,0 +1,11 @@
+require "rails_helper"
+
+RSpec.describe UsersController, type: :controller do
+  context "GET #show" do
+    it "Return for succes respond" do
+      user = User.create!(name: "my_name" , email: "my_name@gmail.com")
+      get :show, params: {id: user.to_params}
+      expect(response).to be_success
+    end
+  end
+end
