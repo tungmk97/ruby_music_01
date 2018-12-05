470e80465e0c32d26c7c8b41af8cf81302467ea3
hct97 <trunghuynh060597@gmail.com>
2018-12-05 15:38:37 +0700

    Refer to task #3926: Delete Comment

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,4 +1,6 @@
 class CommentsController < ApplicationController
+  before_action :correct_user, only: :destroy
+
   def create
     @song = Song.find_by id: params[:song_id]
     @comment = @song.comments.build comment_params
@@ -11,9 +13,23 @@ class CommentsController < ApplicationController
     redirect_to songs_url
   end
 
+  def destroy
+    if @comment.destroy
+      flash[:success] = t ".destroy"
+    else
+      flash[:danger] = t ".cant_destroy"
+    end
+    redirect_to songs_url
+  end
+
   private
 
   def comment_params
     params.require(:comment).permit(:content).merge user: current_user
+  end
+
+  def correct_user
+    @comment = current_user.comments.find_by id: params[:id]
+    redirect_to songs_url unless @comment
   end
 end
