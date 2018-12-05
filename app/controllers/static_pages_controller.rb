2ef0e6f6becc33b8be8c71aa4c412d5687a6b34c
hct97 <trunghuynh060597@gmail.com>
2018-12-05 21:18:25 +0700

    Refer to task #4172: Show Feed

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,3 +1,6 @@
 class StaticPagesController < ApplicationController
-  def home; end
+  def home
+    @feed_items = Song.hot_feed
+                      .page(params[:id]).per Settings.pages.feed
+  end
 end
