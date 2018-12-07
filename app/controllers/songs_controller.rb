98a1710963692d4c6590ea86a8642ee65130f705
hct97 <trunghuynh060597@gmail.com>
2018-12-07 15:42:00 +0700

    Refer to task #3920: Add Song To Playlist

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,20 +1,17 @@
 class SongsController < ApplicationController
+  before_action :find_song, only: :show
   def index
-    @songs = if params[:search]
-               Song.where("title LIKE ?", "%#{params[:search]}%")
-                   .page(params[:page]).per Settings.pages.per_page
-             else
-               Song.page(params[:page]).per Settings.pages.per_page
-             end
+    @songs = Song.search_song(params[:search])
+                 .page(params[:page]).per Settings.pages.per_page
   end
 
   def show
-    @song = Song.include_to_song.find_by id: params[:id]
-    redirect_to songs_url unless @song
     @comments = @song.comments.ordered
                      .page(params[:page]).per Settings.pages.per_page
     @comment = current_user.comments.build if logged_in?
     @genres = @song.genres
+    @playlist = current_user.playlists.order_desc if current_user &&
+                                                     current_user.playlists.any?
   end
 
   private
@@ -25,5 +22,10 @@ class SongsController < ApplicationController
 
   def admin_user
     redirect_to root_url unless current_user.admin?
+  end
+
+  def find_song
+    @song = Song.include_to_song.find_by id: params[:id]
+    redirect_to songs_url unless @song
   end
 end
--- app/models/playlist_song.rb
+++ app/models/playlist_song.rb
@@ -1,4 +1,6 @@
 class PlaylistSong < ApplicationRecord
   belongs_to :playlist
   belongs_to :song
+
+  validates_uniqueness_of :playlist_id, scope: :song_id
 end
